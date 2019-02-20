a08968e5b348bfadb1e2dc9293981e472b2a794e
crisner <christie.reni@gmail.com>
2019-02-20 15:07:26 +0530

    Add routes to signin and signup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import Signin from "./components/auth/Signin";
+import Signup from "./components/auth/Signup";
 
 class App extends Component {
 	render() {
@@ -13,6 +15,8 @@ class App extends Component {
 					<Switch>
 						<Route exact path="/" component={Dashboard} />
 						<Route path="/project/:id" component={ProjectDetails} />
+						<Route path="/signin" component={Signin} />
+						<Route path="/signup" component={Signup} />
 					</Switch>
 				</div>
 			</BrowserRouter>
